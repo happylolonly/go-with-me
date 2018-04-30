804b794428633a5a8c864177fde87de58a0f2981
HappyLoL <happylolonly@gmail.com>
2018-04-30 22:37:44 +0300

    update routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,13 @@
-import List from '../models/list';
+import User from '../models/User';
 const requireLogin = require('../middlewares/requireLogin');
 
 
 export default app => {
-    app.get('/api/user', (req, res) => {
+    app.get('/api/auth', (req, res) => {
+        res.send(req.user ? true : false);
+    });
+
+    app.get('/api/user', requireLogin, (req, res) => {
         res.send(req.user);
-    });   
+    });
 }
