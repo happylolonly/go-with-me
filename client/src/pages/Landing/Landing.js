e8900b4d019bef77116fde98a07d7f98d3c1ca72
Julia Dolgun <siannn@yandex.ru>
2018-05-02 22:58:32 +0300

    added styles for landing page

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router';
+import SocialButtons from '../../components/SocialButtons/SocialButtons';
 
 // import './DashboardContainer.scss';
+import './Landing.scss';
 
 
 class Campaigns extends Component {
@@ -18,18 +20,39 @@ class Campaigns extends Component {
   render() {
     console.log(this.state);
     return (
-        <div className="landing">
-            {/* <h2>Лендинг</h2> */}
+      <div className="landing">
+        {/* <h2>Лендинг</h2> */}
+        <section className="welcome">
+          <div className="welcome__text">
+            <h2 className="welcome__title">Привет!</h2>
 
-            <h2>Привет!</h2>
+            <p>Приложение позволяет создавать списки друзей из&nbsp;разных источников (вк, фб&nbsp;и&nbsp;тд) и&nbsp;потом быстро отправлять всем приглашения на&nbsp;какое нибудь мероприятие</p>
 
-<p>Приложение позволяет создавать списки друзей из разных источников (вк, фб и тд) и потом быстро отправлять всем приглашения на какое нибудь мероприятие</p>
+            <p>Чтобы получать уведомления в&nbsp;вк&nbsp;необходимо чтобы друзья были подписаны <a href="https://vk.com/bot_friendy" target="_blank">на&nbsp;эту группу</a> и&nbsp;первый раз написали ей&nbsp;любое сообщение</p>
+            <p>после чего она будет отправлять сообщения</p>
+          </div>
+        </section>
 
-<p>Чтобы получать уведомления в вк необходимо чтобы друзья были подписаны на эту группу https://vk.com/bot_friendy и первый раз написали ей любое сообщение</p>
-<p>после чего она будет отправлять сообщения</p>
+        <section className="login">
+          <h2 className="login__title">Войти через соц. сети</h2>
+          <SocialButtons />
+        </section>
 
-            <li><a href="/auth/google">Войти через вк</a></li>;
-        </div>
+        <section className="stages">
+          <h2 className="stages__title">Как это работает</h2>
+          <ul class="how-we-work">
+            <li>
+              <p class="how-we-work__title">Один раз добавляешь друзей из соц сетей</p>
+            </li>
+            <li>
+              <p class="how-we-work__title">Создаешь мероприятие и отправляешь его друзьям</p>
+            </li>
+            <li>
+              <p class="how-we-work__title">Все друзья получают уведомления</p>
+            </li>
+          </ul>
+        </section>
+      </div>
     )
   }
 }
