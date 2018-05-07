1dd124242f8adb65bc933a9f8da70012dbee4809
HappyLoL <happylolonly@gmail.com>
2018-05-07 10:49:53 +0300

    add new links

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -27,11 +27,7 @@ class Campaigns extends Component {
         <section className="welcome">
           <div className="welcome__text">
             <h2 className="welcome__title">Привет!</h2>
-
-            <p>Приложение позволяет создавать списки друзей из&nbsp;разных источников (вк, фб&nbsp;и&nbsp;тд) и&nbsp;потом быстро отправлять всем приглашения на&nbsp;какое нибудь мероприятие</p>
-
-            <p>Чтобы получать уведомления в&nbsp;вк&nbsp;необходимо чтобы друзья были подписаны <a href="https://vk.com/bot_friendy" target="_blank">на&nbsp;эту группу</a> и&nbsp;первый раз написали ей&nbsp;любое сообщение</p>
-            <p>после чего она будет отправлять сообщения</p>
+            <p>Приложение позволяет создавать списки друзей из&nbsp;разных источников (вк, фб&nbsp; телеграм&nbsp; тд) и&nbsp;потом быстро отправлять всем приглашения на&nbsp;какое нибудь мероприятие</p>
           </div>
         </section>
 
@@ -39,6 +35,14 @@ class Campaigns extends Component {
           <h2 className="login__title">Войти через соц. сети</h2>
           <a href="/auth/vk" className="vk"><VKIcon size={65} round={true} /></a>
           <a href="/auth/google" className="vk"><GoogleIcon size={65} round={true} /></a>
+        </section>
+
+        <section>
+          <p>Группы на которые необходимо подписаться и отправить первый раз любое сообщение чтобы получать уведомления:</p>
+          <a href="https://vk.com/bot_friendy" target="_blank">Vk</a> 
+          <a href="https://www.facebook.com/Бот-Френди-204430280346743/" target="_blank">Facebook</a>
+          <a href="https://web.telegram.org/#/im?p=@FrendyEventBot" target="_blank">Телеграм</a>
+          <a href="#">Viber (скоро)</a>
         </section>
 
         <section className="stages">
