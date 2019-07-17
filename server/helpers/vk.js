0b64d64f85503bc06c11fe5ac67fe18a0f4d0bfa
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-07-17 11:51:23 +0300

    update vk bot to callback API

--- server/helpers/vk.js
+++ server/helpers/vk.js
@@ -1,144 +1,144 @@
 // import Friend from '../models/friend';
 // import List from '../models/list';
-import User from '../models/User';
+import User from "../models/User";
 
-import Subscriber from '../models/Subscriber';
+import Subscriber from "../models/Subscriber";
 
+import { Bot as Bot2 } from "node-vk-bot";
+const VkBot = require("node-vk-bot-api");
+import { processUser, inviteFriends } from "./s";
 
+import vkId from "./vkId";
 
-import { Bot } from 'node-vk-bot';
+const token =
+  "20a14540148556de06e7fb0c5abee74ae174df47de504ee14f6612eed349bba6a5793d5b03c5e27a04dc4";
 
-import { processUser, inviteFriends } from './s';
+const bot = new VkBot({
+  token: token,
+  // group_id: 165270834
+  confirmation: "e75adbe3"
+  // prefix: /^Bot[\s,]/
+});
 
-
-import vkId from './vkId';
-
-const token = '20a14540148556de06e7fb0c5abee74ae174df47de504ee14f6612eed349bba6a5793d5b03c5e27a04dc4';
-
-const bot = new Bot({
-    token: token,
-    // prefix: /^Bot[\s,]/
-  }).start()
+const bot2 = new Bot2({
+  token: token,
+  group_id: 165270834
+  // confirmation: "e75adbe3"
+  // prefix: /^Bot[\s,]/
+});
 
 //   /Hi|Hello|Hey/i,
 // /^(.|\n)*$/
 
-  bot.get(/./i, async message => {
-    const { user_id: id } = message;
+bot.on(async ctx => {
+  // /./i
+  const { message } = ctx;
+  const { user_id: id } = message;
 
-    const users = await bot.api('users.get', { user_ids: id });
+  const users = await bot2.api("users.get", { user_ids: id });
 
-    const { first_name: firstName, last_name: lastName } = users[0];
+  const { first_name: firstName, last_name: lastName } = users[0];
 
-    const existingUser = await Subscriber.findOne({ id, source: 'vk' });
+  const existingUser = await Subscriber.findOne({ id, source: "vk" });
 
-    if (existingUser) {
-
-      const phraces = [
-        'У меня пока нету нейросетей - не могу ответить тебе что-нибудь по делу',
-        'Я рад что ты мне пишешь, но лучше напиши тому кто тебя позвал на мероприятие',
-        '!@#$%^&*@%&%#@#@&#@$^#@',
-        'Эй, не сломай меня!'
-      ];
-
-      bot.send(phraces[Math.floor(Math.random() * phraces.length)], message.peer_id);
-      await processUser(id, { firstName, lastName }, 'vk'); // пересохранить
-    } else {
-      await processUser(id, { firstName, lastName }, 'vk');
-      bot.send('Привет! Круто что ты мне написал! Я тебя сохранил, теперь ты можешь получать приглашения от друзей!', message.peer_id);
-    }
-
-  });
-
-
-
-function greeting() {
-    const gr = [
-        'Хай',
-        'Привет',
-        'Хеллоу',
-        'Нихао',
-        'Прив',
-        'Здаров',
+  if (existingUser) {
+    const phraces = [
+      "У меня пока нету нейросетей - не могу ответить тебе что-нибудь по делу",
+      "Я рад что ты мне пишешь, но лучше напиши тому кто тебя позвал на мероприятие",
+      "!@#$%^&*@%&%#@#@&#@$^#@",
+      "Эй, не сломай меня!"
     ];
 
-    const random = Math.floor(Math.random() * gr.length);
+    ctx.reply(
+      phraces[Math.floor(Math.random() * phraces.length)],
+      message.peer_id
+    );
+    await processUser(id, { firstName, lastName }, "vk"); // пересохранить
+  } else {
+    await processUser(id, { firstName, lastName }, "vk");
+    ctx.reply(
+      "Привет! Круто что ты мне написал! Я тебя сохранил, теперь ты можешь получать приглашения от друзей!",
+      message.peer_id
+    );
+  }
+});
 
-    return gr[random];
+function greeting() {
+  const gr = ["Хай", "Привет", "Хеллоу", "Нихао", "Прив", "Здаров"];
+
+  const random = Math.floor(Math.random() * gr.length);
+
+  return gr[random];
 }
 
 export default {
-    async send({title, description, link, list, id}) {
+  async send({ title, description, link, list, id }) {
+    return new Promise(async (resolve, reject) => {
+      const user = (await User.findById(id)).toObject();
 
-        return new Promise( async (resolve, reject) => {
+      const lists = user.lists;
 
-            const user = (await User.findById(id)).toObject();
+      const listObj = lists.filter(item => {
+        return item._id.toString() === list;
+      })[0];
 
+      // const listObj = await List.findById(list);
 
-            const lists = user.lists;
+      const friends = [];
 
-            const listObj = lists.filter(item => {
-                return item._id.toString() === list
-            })[0];
+      for (let item of listObj.friends) {
+        // const friend = await Friend.findById(item);
+        const friend = user.friends.filter(
+          item2 => item2._id.toString() === item
+        )[0];
 
+        // убрать костыль перенеся проверку на фронт
+        if (!friend) continue;
 
-            // const listObj = await List.findById(list);
+        if (friend.source !== "vk") continue;
 
-            const friends = [];
+        if (
+          friend.link.includes("/id") &&
+          Number.isInteger(+friend.link.split("/id")[1])
+        ) {
+          friend.link = friend.link.split("id")[1];
+        } else {
+          const id = await vkId(friend.link.split("vk.com/")[1]);
+          friend.link = id.id;
+        }
+        friends.push(friend.link);
+      }
 
-            for (let item of listObj.friends) {
-                // const friend = await Friend.findById(item);
-                const friend = user.friends.filter(item2 => item2._id.toString() === item)[0];
+      let friend = "";
 
-                // убрать костыль перенеся проверку на фронт
-                if (!friend) continue;
+      if (user.login.type === "vk");
+      {
+        const d = await vkId(user.login.id);
+        const { first_name, last_name, id } = d;
 
-                if (friend.source !== 'vk') continue;
+        friend = `@id${id} (${first_name} ${last_name})`;
+      }
 
-
-                if (friend.link.includes('/id') && Number.isInteger(+friend.link.split('/id')[1])) {
-                    friend.link = friend.link.split('id')[1];
-                } else {
-                    const id = await vkId(friend.link.split('vk.com/')[1]);
-                    friend.link = id.id;
-                }
-                friends.push(friend.link)
-
-            }
-
-
-            let friend = '';
-
-            if (user.login.type === 'vk'); {
-                const d = await vkId(user.login.id);
-                const { first_name, last_name, id } = d;
-
-                friend = `@id${id} (${first_name} ${last_name})`;
-            }
-
-            friends.forEach(item => {
-
-                bot.send(`
+      friends.forEach(item => {
+        bot.send(
+          `
                     ${greeting()}! Твой друг ${friend} зовет тебя на ${title}! \n ${description} \n Ссылка: ${link} ${inviteFriends()}
-                `.trim(), item);
-            });
+                `.trim(),
+          item
+        );
+      });
 
-            resolve();
+      resolve();
+    });
 
-        });
+    // bot.get(/Hi|Hello|Hey/i, message => {
+    //   const options =  { forward_messages: message.id }
 
+    //   console.log(message.id)
 
+    //   bot.send('Hello!', message.peer_id, options)
+    // })
+  },
 
-// bot.get(/Hi|Hello|Hey/i, message => {
-//   const options =  { forward_messages: message.id }
-
-
-//   console.log(message.id)
-
-//   bot.send('Hello!', message.peer_id, options)
-// })
-
-
-
-    }
-}
+  bot
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,150 +1,129 @@
-import Subscriber from '../models/Subscriber';
-import axios from 'axios';
-import fb from '../helpers/fb';
+import Subscriber from "../models/Subscriber";
+import axios from "axios";
+import fb from "../helpers/fb";
 
+import vk from "../helpers/vk";
 
-export default (app) => {
+export default app => {
+  app.get("/", (req, res, next) => {
+    // if (req.originalUrl.slice(-1) != '/') return next();
+    // res.sendFile(__dirname + '/../static/build/index.html');
+  });
 
-    app.get('/', (req, res, next) => {
-        // if (req.originalUrl.slice(-1) != '/') return next();
-        // res.sendFile(__dirname + '/../static/build/index.html');
-    });
+  require("./events").default(app);
+  require("./friends").default(app);
+  require("./lists").default(app);
+  require("./auth")(app);
+  require("./users").default(app);
+  // require('./logs')(app);
 
+  // Adds support for GET requests to our webhook
+  app.get("/webhook", (req, res) => {
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "12345";
 
+    // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
 
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Checks the mode and token sent is correct
+      if (mode === "subscribe" && token === VERIFY_TOKEN) {
+        // Responds with the challenge token from the request
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+      }
+    }
+  });
 
-    require('./events').default(app);
-    require('./friends').default(app);
-    require('./lists').default(app);
-    require('./auth')(app);
-    require('./users').default(app);
-    // require('./logs')(app);
+  app.post("/vk-bot", vk.bot.webhookCallback);
 
+  app.post("/webhook", async (req, res) => {
+    let body = req.body;
 
-    // Adds support for GET requests to our webhook
-    app.get('/webhook', (req, res) => {
+    axios.post(`${process.env.LOCALTUNNEL}`, body);
 
-        // Your verify token. Should be a random string.
-        let VERIFY_TOKEN = "12345"
+    // body = JSON.parse('"{"object":"page","entry":[{"id":"204430280346743","time":1525641387754,"messaging":[{"sender":{"id":"1673239386087268"},"recipient":{"id":"204430280346743"},"timestamp":1525632959921,"message":{"mid":"mid.$cAADjHnreAc1paBTNsVjNs-IlZiQH","seq":2786,"text":"sss"}}]}]}"');
 
-        // Parse the query params
-        let mode = req.query['hub.mode'];
-        let token = req.query['hub.verify_token'];
-        let challenge = req.query['hub.challenge'];
+    // Checks this is an event from a page subscription
+    if (body.object === "page") {
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(async function(entry) {
+        // Gets the message. entry.messaging is an array, but
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+        const { id } = webhook_event.sender;
+        const text = webhook_event.message.text;
 
-        // Checks if a token and mode is in the query string of the request
-        if (mode && token) {
+        const profile = await fb.getProfile(id);
 
-            // Checks the mode and token sent is correct
-            if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        handleUser(text, profile.data, id, ({ text }) => {
+          fb.sendMessage(id, text);
+        });
+      });
 
-                // Responds with the challenge token from the request
-                console.log('WEBHOOK_VERIFIED');
-                res.status(200).send(challenge);
-
-            } else {
-                // Responds with '403 Forbidden' if verify tokens do not match
-                res.sendStatus(403);
-            }
-        }
-    });
-
-
-    app.post('/webhook', async (req, res) => {
-
-        let body = req.body;
-
-        axios.post(`${process.env.LOCALTUNNEL}`, body);
-
-        // body = JSON.parse('"{"object":"page","entry":[{"id":"204430280346743","time":1525641387754,"messaging":[{"sender":{"id":"1673239386087268"},"recipient":{"id":"204430280346743"},"timestamp":1525632959921,"message":{"mid":"mid.$cAADjHnreAc1paBTNsVjNs-IlZiQH","seq":2786,"text":"sss"}}]}]}"');
-
-
-        // Checks this is an event from a page subscription
-        if (body.object === 'page') {
-
-            // Iterates over each entry - there may be multiple if batched
-            body.entry.forEach(async function (entry) {
-
-                // Gets the message. entry.messaging is an array, but 
-                // will only ever contain one message, so we get index 0
-                let webhook_event = entry.messaging[0];
-                console.log(webhook_event);
-                const { id } = webhook_event.sender;
-                const text = webhook_event.message.text;
-
-                const profile = await fb.getProfile(id);
-
-                handleUser(text, profile.data, id, ({text}) => {
-                    fb.sendMessage(id, text);
-                });
-
-            });
-
-            // Returns a '200 OK' response to all requests
-            res.status(200).send('EVENT_RECEIVED');
-        } else {
-            // Returns a '404 Not Found' if event is not from a page subscription
-            res.sendStatus(404);
-        }
-
-    });
-}
+      // Returns a '200 OK' response to all requests
+      res.status(200).send("EVENT_RECEIVED");
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  });
+};
 
 async function handleUser(text, profile, chatId, done) {
-    const { first_name: firstName, last_name: lastName } = profile;
+  const { first_name: firstName, last_name: lastName } = profile;
 
-    if (text.includes('https://www.facebook.com/')) {
-        const username = text.split('https://www.facebook.com/')[1];
+  if (text.includes("https://www.facebook.com/")) {
+    const username = text.split("https://www.facebook.com/")[1];
 
-        const id = await getId(username);
+    const id = await getId(username);
 
-        const existingUser = await Subscriber.findOne({ id: id, source: 'facebook' });
+    const existingUser = await Subscriber.findOne({
+      id: id,
+      source: "facebook"
+    });
 
-        if (existingUser) {
-
-            await Subscriber.findByIdAndUpdate(existingUser._id, {
-                firstName,
-                lastName,
-                userName: username,
-                chatId
-            });
-
-        } else {
-
-            await new Subscriber({
-                id: id,
-                chatId,
-                firstName,
-                lastName,
-                userName: username,
-                source: 'facebook'
-            }).save();
-
-        }
-        done({ text: 'Спасибо, мы тебя сохранили!' });
-
+    if (existingUser) {
+      await Subscriber.findByIdAndUpdate(existingUser._id, {
+        firstName,
+        lastName,
+        userName: username,
+        chatId
+      });
     } else {
-        done({ text: 'Привет, отправь плз свою ссылку на профиль' });
+      await new Subscriber({
+        id: id,
+        chatId,
+        firstName,
+        lastName,
+        userName: username,
+        source: "facebook"
+      }).save();
     }
+    done({ text: "Спасибо, мы тебя сохранили!" });
+  } else {
+    done({ text: "Привет, отправь плз свою ссылку на профиль" });
+  }
 }
-
-
 
 async function getId(name) {
+  return new Promise(async resolve => {
+    let page = await axios.get(`https://www.facebook.com/${name}`);
 
-    return new Promise(async resolve => {
+    page = page.data;
 
-        let page = await axios.get(`https://www.facebook.com/${name}`);
+    const s = page.indexOf("entity_id");
+    const e = page.indexOf("&", s);
 
-        page = page.data;
+    const id = page.substring(s + 10, e);
 
-        const s = page.indexOf('entity_id');
-        const e = page.indexOf('&', s);
-
-        const id = page.substring(s + 10, e);
-
-        resolve(id);
-
-    });
+    resolve(id);
+  });
 }
-
